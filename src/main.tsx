bf5b405a061fcab4010a6d5b9a26d2a572f23f64
Maksym Hrytsenko <mahrywork@gmail.com>
2026-07-10 12:25:33 +0100

    feat: add typography styles and font source; update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,10 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <div style={{ padding: 24 }}>
-            <div className="bg-surface-default" style={{ padding: 24 }}>
+            <div
+                className="bg-surface-default text-display-xl"
+                style={{ padding: 24 }}
+            >
                 surface/default
             </div>
             <div className="bg-surface-subtle" style={{ padding: 24 }}>
